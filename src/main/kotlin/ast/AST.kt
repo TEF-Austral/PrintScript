ba80e7c9ad86a89089a5b4642e2b94c394a815db
Franco <fdescalzo@mail.austral.edu.ar>
2025-08-11 15:58:00 -0300

    terminado los codigos del AST, Traverser, Node.

--- src/main/kotlin/ast/AST.kt
+++ src/main/kotlin/ast/AST.kt
@@ -1,16 +1,13 @@
 package ast
 
 sealed interface AST {
-    fun addNode(node: Node): AST
+    fun getRoot(): Node
     fun traverse(): List<Node>
 }
 
-class PrintScriptAST(private val traverser: Traverser): AST {
+class PrintScriptAST(private val traverser: Traverser, private val root: Node): AST {
     override fun traverse(): List<Node> {
         return traverser.traverse(this)
     }
-
-    override fun addNode(node: Node): AST {
-        TODO("Not yet implemented")
-    }
+    override fun getRoot(): Node {return root}
 }
--- src/main/kotlin/ast/Node.kt
+++ src/main/kotlin/ast/Node.kt
@@ -4,18 +4,18 @@ import token.Token
 
 sealed interface Node {
     fun getValue() : Token
-    fun left() : Node
-    fun right() : Node
+    fun left() : Node?
+    fun right() : Node?
 }
 
-class PrintScriptNode(private val token: Token, private val leftNode: Node, private val rightNode: Node) : Node {
+class PrintScriptNode(private val token: Token, private val leftNode: Node?, private val rightNode: Node?) : Node {
     override fun getValue() : Token {
         return token
     }
-    override fun left() : Node {
+    override fun left() : Node? {
         return leftNode
     }
-    override fun right() : Node {
+    override fun right() : Node? {
         return rightNode
     }
 }
