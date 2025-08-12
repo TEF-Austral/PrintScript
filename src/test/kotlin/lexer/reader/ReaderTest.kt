f61d743f224b6cf893e22c3c82e1463d4c5a39a4
Tomas M <tjmonteiro@mail.austral.edu.ar>
2025-08-12 10:11:36 -0300

    refactor TxtReader and add tests; improve error handling and validate file reading functionality

--- src/test/kotlin/lexer/reader/ReaderTest.kt
+++ src/test/kotlin/lexer/reader/ReaderTest.kt
@@ -1,14 +1,28 @@
 package lexer.reader
 
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.io.TempDir
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertThrows
-import java.nio.file.Path
-import java.io.IOException
-import kotlin.io.path.writeText
+import java.io.File
 
 class ReaderTest {
 
+    @Test
+    fun `TxtReader should read content from a valid file`() {
+        val filePath = "src/test/resources/script.txt"
+        val expectedContent = File(filePath).readText()
 
+        val reader = TxtReader(filePath)
+        val actualContent = reader.read()
+        print(expectedContent)
+
+        assertEquals(expectedContent, actualContent)
+    }
+
+    @Test
+    fun `TxtReader should return an empty String for a non-existent file`() {
+        val nonExistentFilePath = "non/existent/file.txt"
+        val reader = TxtReader(nonExistentFilePath)
+
+        assertEquals("", reader.read())
+    }
 }
