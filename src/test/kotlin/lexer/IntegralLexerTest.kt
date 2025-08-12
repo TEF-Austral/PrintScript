090caa023fc36439ba28c51f8f8fb720c608f91e
Tomas M <tjmonteiro@mail.austral.edu.ar>
2025-08-12 09:50:10 -0300

    add tests for IntegralLexer and TokenConverter; implement basic structure and initial test cases

---
+++ src/test/kotlin/lexer/IntegralLexerTest.kt
@@ -0,0 +1,7 @@
+package lexer
+
+import org.junit.jupiter.api.Test
+
+
+class IntegralLexerTest {
+}
--- src/test/kotlin/lexer/reader/ReaderTest.kt
+++ src/test/kotlin/lexer/reader/ReaderTest.kt
@@ -10,27 +10,5 @@ import kotlin.io.path.writeText
 
 class ReaderTest {
 
-    @TempDir
-    lateinit var tempDir: Path
 
-    @Test
-    fun `read returns file content`() {
-        val file = tempDir.resolve("sample.txt")
-        file.writeText("Hello, PrintScript!")
-        val reader = TxtReader(file.toString())
-
-        val content = reader.read()
-
-        assertEquals("Hello, PrintScript!", content)
-    }
-
-    @Test
-    fun `read non existing file throws IOException`() {
-        val missingPath = tempDir.resolve("missing.txt").toString()
-        val reader = TxtReader(missingPath)
-
-        assertThrows(IOException::class.java) {
-            reader.read()
-        }
-    }
 }
