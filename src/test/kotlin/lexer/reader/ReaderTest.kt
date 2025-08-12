27027c391625f2d2cbbc3a37d81355a586ba3bd2
Franco <fdescalzo@mail.austral.edu.ar>
2025-08-12 10:14:59 -0300

    changes de readerTest, need to merge

--- src/test/kotlin/lexer/reader/ReaderTest.kt
+++ src/test/kotlin/lexer/reader/ReaderTest.kt
@@ -1,12 +1,5 @@
 package lexer.reader
 
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.io.TempDir
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertThrows
-import java.nio.file.Path
-import java.io.IOException
-import kotlin.io.path.writeText
 
 class ReaderTest {
 
