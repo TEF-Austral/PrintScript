356b993d2f3c7cc3af582b278a28898dadcb9192
Franco <fdescalzo@mail.austral.edu.ar>
2025-08-15 13:40:49 -0300

    parser in progress

--- parser/src/main/kotlin/Parser.kt
+++ parser/src/main/kotlin/Parser.kt
@@ -1,5 +1,7 @@
 import builder.NodeBuilder
+import node.BlockStatement
 import node.Program
+import node.Statement
 
 sealed interface Parser {
 
@@ -8,39 +10,84 @@ sealed interface Parser {
 
 }
 
-class DefaultParser(private val nodeBuilder: NodeBuilder) : Parser {
+//let x = 42 ;
+//let a = 0;
+//println(x) ;
+// a = 3;
+// a + 3;
+// 3 + 3 + 3; -> Declaration
+// 3 + 3
 
+
+class DefaultParser(private val nodeBuilder: NodeBuilder) : Parser {
     override fun parse(tokens: List<Token>): Program {
-        for (token in tokens) {
-            // BlockStatement
-            when (token.getType()) {
-                TokenType.DECLARATION -> {
-                    // Handle variable declaration
-                }
-                TokenType.ASSIGNMENT -> {
-                    // Handle assignment
-                }
-                TokenType.FUNCTION -> {
-                    // Handle function declaration
-                }
-                TokenType.PRINT -> {
-                    // Handle print statement
-                }
-                TokenType.IDENTIFIER -> {
-                    // Handle identifier usage
-                }
-                TokenType.NUMBER_LITERAL, TokenType.STRING_LITERAL -> {
-                    // Handle literals
-                }
-                else -> {
-                    // Handle other tokens or throw an error
+        val statements = mutableListOf<Statement>()
+        for (block in delimiterSplit(tokens)) {
+            for (token in block) {
+                when (token.getType()) {
+                    TokenType.IDENTIFIER -> {
+                        identifierParser(block)
+                    }
+                    TokenType.DECLARATION -> {
+                        declarationParser(block)
+                    }
+                    TokenType.PRINT -> {
+                        printParser(block)
+                    }
+                    else -> {
+                        expressionParser(block)
+                    }
                 }
             }
         }
-
+        return nodeBuilder.buildProgramNode(statements)
     }
 
+    private fun identifierParser(block: List<Token>): Statement {
+    }
+    private fun declarationParser(block: List<Token>): Statement {
+        nodeBuilder.buildVariableDeclarationStatementNode(block.first())
+    }
+    private fun printParser(tokens: List<Token>): Statement {
+       for (token in tokens) {
+           if (tokens.size == 4){
+               val literalNode = nodeBuilder.buildLiteralExpressionNode(token)
+               return nodeBuilder.buildPrintStatementNode(literalNode)
+           }
+           else{
+                val expression = expressionParser(tokens[1 until tokens.size - 1])
+                return nodeBuilder.buildPrintStatementNode(expression)
+           }
+        }
+    }
 
+    private fun expressionParser(tokens: List<Token>): Program {
+        TODO(" A  IMPLEMENTAR")
+    }
+
+    // [3 + 3 + 3];
+    // [ 3 + 3 ] + 3;
+    // print(3 + 3 + 3);
+    // Par expression, Impar
+    // print("algo") // lenth 4
+
+
+    private fun delimiterSplit(tokens: List<Token>): List<List<Token>>  {
+        val result = mutableListOf<List<Token>>()
+        for (token in tokens) {
+            val tempList = mutableListOf<Token>()
+            when (token.getType()) {
+                TokenType.DELIMITERS -> {
+                    result.add(tempList)
+                }
+                else -> {
+                    tempList.add(token)
+                }
+            }
+        }
+        return result
+    }
 }
+
 
 
