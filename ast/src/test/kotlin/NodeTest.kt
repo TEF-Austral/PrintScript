4f5d678ba38e8a30334db470e54c01261311a85f
Franco <fdescalzo@mail.austral.edu.ar>
2025-08-17 12:30:06 -0300

    Added node and parser test. May need more extensive testing

---
+++ ast/src/test/kotlin/NodeTest.kt
@@ -0,0 +1,175 @@
+package node
+
+import Position
+import PrintScriptToken
+import Token
+import builder.DefaultNodeBuilder
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.BeforeEach
+
+class NodeTest {
+
+    private lateinit var nodeBuilder: DefaultNodeBuilder
+    private lateinit var mockToken: Token
+    private lateinit var numberToken: Token
+    private lateinit var identifierToken: Token
+    private lateinit var operatorToken: Token
+    private lateinit var dataTypeToken: Token
+
+    @BeforeEach
+    fun setUp() {
+        nodeBuilder = DefaultNodeBuilder()
+
+        // Create mock tokens for testing
+        val coordinates = Position(1, 1)
+        mockToken = PrintScriptToken(TokenType.STRING_LITERAL, "test", coordinates)
+        numberToken = PrintScriptToken(TokenType.NUMBER_LITERAL, "42", coordinates)
+        identifierToken = PrintScriptToken(TokenType.IDENTIFIER, "myVar", coordinates)
+        operatorToken = PrintScriptToken(TokenType.OPERATORS, "+", coordinates)
+        dataTypeToken = PrintScriptToken(TokenType.DATA_TYPES, "NUMBER", coordinates)
+    }
+
+    @Test
+    fun testLiteralExpression() {
+        val literalExpr = nodeBuilder.buildLiteralExpressionNode(mockToken)
+
+        assertEquals("test", literalExpr.getValue())
+        assertNotNull(literalExpr)
+    }
+
+    @Test
+    fun testIdentifierExpression() {
+        val identifierExpr = nodeBuilder.buildIdentifierNode(identifierToken)
+
+        assertEquals("myVar", identifierExpr.getName())
+        assertNotNull(identifierExpr)
+    }
+
+    @Test
+    fun testBinaryExpression() {
+        val leftExpr = nodeBuilder.buildLiteralExpressionNode(numberToken)
+        val rightExpr = nodeBuilder.buildIdentifierNode(identifierToken)
+
+        val binaryExpr = nodeBuilder.buildBinaryExpressionNode(leftExpr, operatorToken, rightExpr)
+
+        assertEquals(leftExpr, binaryExpr.getLeft())
+        assertEquals(operatorToken, binaryExpr.getOperator())
+        assertEquals(rightExpr, binaryExpr.getRight())
+    }
+
+    @Test
+    fun testVariableDeclarationStatement() {
+        val initialValue = nodeBuilder.buildLiteralExpressionNode(numberToken)
+
+        val varDecl = nodeBuilder.buildVariableDeclarationStatementNode(
+            identifierToken,
+            dataTypeToken,
+            initialValue
+        )
+
+        assertEquals("myVar", varDecl.getIdentifier())
+        assertEquals("NUMBER", varDecl.getDataType())
+        assertEquals(initialValue, varDecl.getInitialValue())
+    }
+
+    @Test
+    fun testVariableDeclarationStatementWithoutInitialValue() {
+        val varDecl = nodeBuilder.buildVariableDeclarationStatementNode(
+            identifierToken,
+            dataTypeToken
+        )
+
+        assertEquals("myVar", varDecl.getIdentifier())
+        assertEquals("NUMBER", varDecl.getDataType())
+        assertNull(varDecl.getInitialValue())
+    }
+
+    @Test
+    fun testAssignmentStatement() {
+        val value = nodeBuilder.buildLiteralExpressionNode(numberToken)
+
+        val assignment = nodeBuilder.buildAssignmentStatementNode(identifierToken, value)
+
+        assertEquals("myVar", assignment.getIdentifier())
+        assertEquals(value, assignment.getValue())
+    }
+
+    @Test
+    fun testPrintStatement() {
+        val expression = nodeBuilder.buildLiteralExpressionNode(mockToken)
+
+        val printStmt = nodeBuilder.buildPrintStatementNode(expression)
+
+        assertEquals(expression, printStmt.getExpression())
+    }
+
+    @Test
+    fun testExpressionStatement() {
+        val expression = nodeBuilder.buildIdentifierNode(identifierToken)
+
+        val exprStmt = nodeBuilder.buildExpressionStatementNode(expression)
+
+        assertEquals(expression, exprStmt.getExpression())
+    }
+
+    @Test
+    fun testEmptyStatement() {
+        val emptyStmt = nodeBuilder.buildEmptyStatementNode()
+
+        assertNotNull(emptyStmt)
+        assertEquals("EmptyStatement", emptyStmt.toString())
+    }
+
+    @Test
+    fun testProgram() {
+        val stmt1 = nodeBuilder.buildEmptyStatementNode()
+        val stmt2 = nodeBuilder.buildPrintStatementNode(
+            nodeBuilder.buildLiteralExpressionNode(mockToken)
+        )
+        val statements = listOf(stmt1, stmt2)
+
+        val program = nodeBuilder.buildProgramNode(statements)
+
+        assertEquals(2, program.getStatements().size)
+        assertEquals(statements, program.getStatements())
+    }
+
+    @Test
+    fun testEmptyExpression() {
+        val emptyExpr = EmptyExpression()
+
+        assertEquals("EmptyExpression", emptyExpr.toString())
+    }
+
+    @Test
+    fun testComplexAST() {
+        // Build a more complex AST: let x: NUMBER = 5 + 10;
+        val leftOperand = nodeBuilder.buildLiteralExpressionNode(
+            PrintScriptToken(TokenType.NUMBER_LITERAL, "5", Position(1, 15))
+        )
+        val rightOperand = nodeBuilder.buildLiteralExpressionNode(
+            PrintScriptToken(TokenType.NUMBER_LITERAL, "10", Position(1, 19))
+        )
+        val binaryExpr = nodeBuilder.buildBinaryExpressionNode(leftOperand, operatorToken, rightOperand)
+
+        val varDecl = nodeBuilder.buildVariableDeclarationStatementNode(
+            PrintScriptToken(TokenType.IDENTIFIER, "x", Position(1, 5)),
+            PrintScriptToken(TokenType.DATA_TYPES, "NUMBER", Position(1, 8)),
+            binaryExpr
+        )
+
+        val program = nodeBuilder.buildProgramNode(listOf(varDecl))
+
+        assertEquals(1, program.getStatements().size)
+        val statement = program.getStatements()[0] as VariableDeclarationStatement
+        assertEquals("x", statement.getIdentifier())
+        assertEquals("NUMBER", statement.getDataType())
+        assertNotNull(statement.getInitialValue())
+
+        val initialValueBinary = statement.getInitialValue() as BinaryExpression
+        assertEquals("5", (initialValueBinary.getLeft() as LiteralExpression).getValue())
+        assertEquals("+", initialValueBinary.getOperator().getValue())
+        assertEquals("10", (initialValueBinary.getRight() as LiteralExpression).getValue())
+    }
+}
--- parser/src/test/kotlin/ParserTest.kt
+++ parser/src/test/kotlin/ParserTest.kt
@@ -1,2 +1,98 @@
+import builder.DefaultNodeBuilder
+import node.*
+import parser.factory.RecursiveDescentParserFactory
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
 class ParserTest {
+
+    @Test
+    fun testSingleVariableDeclaration() {
+        val tokens = listOf(
+            PrintScriptToken(TokenType.DECLARATION, "let", Position(1, 1)),
+            PrintScriptToken(TokenType.IDENTIFIER, "x", Position(1, 5)),
+            PrintScriptToken(TokenType.DELIMITERS, ":", Position(1, 7)),
+            PrintScriptToken(TokenType.DATA_TYPES, "NUMBER", Position(1, 9)),
+            PrintScriptToken(TokenType.ASSIGNMENT, "=", Position(1, 16)),
+            PrintScriptToken(TokenType.NUMBER_LITERAL, "5", Position(1, 18)),
+            PrintScriptToken(TokenType.DELIMITERS, ";", Position(1, 19))
+        )
+
+        val nodeBuilder = DefaultNodeBuilder()
+        val parser = RecursiveDescentParserFactory().createParser(tokens, nodeBuilder)
+        val program = parser.parse()
+
+        assertEquals(1, program.getStatements().size)
+        val statement = program.getStatements()[0]
+        assertTrue(statement is VariableDeclarationStatement)
+
+        val varDecl = statement as VariableDeclarationStatement
+        assertEquals("x", varDecl.getIdentifier())
+        assertEquals("NUMBER", varDecl.getDataType())
+        assertTrue(varDecl.getInitialValue() is LiteralExpression)
+
+        val literal = varDecl.getInitialValue() as LiteralExpression
+        assertEquals("5", literal.getValue())
+    }
+
+    @Test
+    fun testMultipleStatementsWithBinaryExpression() {
+        val tokens = listOf(
+            PrintScriptToken(TokenType.DECLARATION, "let", Position(1, 1)),
+            PrintScriptToken(TokenType.IDENTIFIER, "x", Position(1, 5)),
+            PrintScriptToken(TokenType.DELIMITERS, ":", Position(1, 7)),
+            PrintScriptToken(TokenType.DATA_TYPES, "NUMBER", Position(1, 9)),
+            PrintScriptToken(TokenType.ASSIGNMENT, "=", Position(1, 16)),
+            PrintScriptToken(TokenType.NUMBER_LITERAL, "5", Position(1, 18)),
+            PrintScriptToken(TokenType.DELIMITERS, ";", Position(1, 19)),
+
+            PrintScriptToken(TokenType.DECLARATION, "let", Position(2, 1)),
+            PrintScriptToken(TokenType.IDENTIFIER, "y", Position(2, 5)),
+            PrintScriptToken(TokenType.DELIMITERS, ":", Position(2, 7)),
+            PrintScriptToken(TokenType.DATA_TYPES, "NUMBER", Position(2, 9)),
+            PrintScriptToken(TokenType.ASSIGNMENT, "=", Position(2, 16)),
+            PrintScriptToken(TokenType.NUMBER_LITERAL, "5", Position(2, 18)),
+            PrintScriptToken(TokenType.DELIMITERS, ";", Position(2, 19)),
+
+            PrintScriptToken(TokenType.IDENTIFIER, "x", Position(3, 1)),
+            PrintScriptToken(TokenType.OPERATORS, "+", Position(3, 2)),
+            PrintScriptToken(TokenType.IDENTIFIER, "y", Position(3, 3)),
+            PrintScriptToken(TokenType.DELIMITERS, ";", Position(3, 4))
+        )
+
+        val nodeBuilder = DefaultNodeBuilder()
+        val parser = RecursiveDescentParserFactory().createParser(tokens, nodeBuilder)
+        val program = parser.parse()
+
+        assertEquals(3, program.getStatements().size)
+
+        val firstStatement = program.getStatements()[0]
+        assertTrue(firstStatement is VariableDeclarationStatement)
+        val firstVarDecl = firstStatement as VariableDeclarationStatement
+        assertEquals("x", firstVarDecl.getIdentifier())
+        assertEquals("NUMBER", firstVarDecl.getDataType())
+        assertEquals("5", (firstVarDecl.getInitialValue() as LiteralExpression).getValue())
+
+        val secondStatement = program.getStatements()[1]
+        assertTrue(secondStatement is VariableDeclarationStatement)
+        val secondVarDecl = secondStatement as VariableDeclarationStatement
+        assertEquals("y", secondVarDecl.getIdentifier())
+        assertEquals("NUMBER", secondVarDecl.getDataType())
+        assertEquals("5", (secondVarDecl.getInitialValue() as LiteralExpression).getValue())
+
+        val thirdStatement = program.getStatements()[2]
+        assertTrue(thirdStatement is ExpressionStatement)
+        val exprStatement = thirdStatement as ExpressionStatement
+        assertTrue(exprStatement.getExpression() is BinaryExpression)
+
+        val binaryExpr = exprStatement.getExpression() as BinaryExpression
+        assertTrue(binaryExpr.getLeft() is IdentifierExpression)
+        assertTrue(binaryExpr.getRight() is IdentifierExpression)
+        assertEquals("+", binaryExpr.getOperator().getValue())
+
+        val leftIdentifier = binaryExpr.getLeft() as IdentifierExpression
+        val rightIdentifier = binaryExpr.getRight() as IdentifierExpression
+        assertEquals("x", leftIdentifier.getName())
+        assertEquals("y", rightIdentifier.getName())
+    }
 }
