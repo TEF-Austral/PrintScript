50704007959a933d8b2f373737f850c219b5fed9
Tomas M <tjmonteiro@mail.austral.edu.ar>
2025-08-25 16:09:15 -0300

    Pusheo Jacoco y correciones menores

--- parser/src/main/kotlin/result/ParseResult.kt
+++ parser/src/main/kotlin/result/ParseResult.kt
@@ -1,7 +1,5 @@
 package parser.result
 
-import Coordinates
-
 sealed class ParseResult<out T> {
     data class Success<out T>(
         val value: T,
@@ -11,8 +9,3 @@ sealed class ParseResult<out T> {
         val error: ParseError,
     ) : ParseResult<Nothing>()
 }
-
-data class ParseError(
-    val message: String,
-    val position: Coordinates,
-)
