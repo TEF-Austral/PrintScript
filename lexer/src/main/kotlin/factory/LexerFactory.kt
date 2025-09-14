5b289199b7c17ca9806b99f13862bfa267892091
Tomas M <tjmonteiro@mail.austral.edu.ar>
2025-09-14 18:09:43 -0300

    Enhance LexerFactory to support buffer size in lexer creation methods

--- lexer/src/main/kotlin/factory/LexerFactory.kt
+++ lexer/src/main/kotlin/factory/LexerFactory.kt
@@ -3,26 +3,31 @@ package factory
 import Lexer
 import converter.specific.StringToTokenConverter
 import java.io.Reader
+import kotlin.Int
 import type.Version
 
 interface LexerFactory {
-    fun createVersionOne(reader: Reader): Lexer
+    fun createVersionOne(reader: Reader,size: Int,): Lexer
 
-    fun createVersionOnePointOne(reader: Reader): Lexer
+    fun createVersionOnePointOne(reader: Reader,size: Int = 65536): Lexer
 
     fun createCustom(
         specialChars: List<Char>,
         customConverters: List<StringToTokenConverter>,
         reader: Reader,
+        size: Int = 65536
     ): Lexer
 
     fun createLexerWithVersion(
         version: Version,
         reader: Reader,
+        size: Int = 65536,
+
     ): Lexer
 
     fun createLexerWithVersionAndBufferSize(
         reader: Reader,
-        size: Int,
+        size: Int = 65536,
+        version: Version,
     ): Lexer
 }
